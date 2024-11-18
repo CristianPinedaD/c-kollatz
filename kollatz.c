2841ae539c66e3a84c085238146fd5c33eccd10c
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-18 11:47:46 -0500

    Final implementation

--- kollatz.c
+++ kollatz.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+// Refresher on the kollatz conjecture:
+
+/*
+
+    We start with a number > 1.
+
+        If the number is even, we divide it by two
+
+        If it is odd, we multiply it by 3 and add 1. 
+
+    That is one step.
+
+    Repeat and count steps until we reach 1.
+
+*/
+
+
+int isEven(int num) {
+    if (num % 2 == 0) {
+		return 1;
+	}
+    
+    else {
+		return 0; 
+	}
+}
+
+int kollatz(int num) {
+
+    int steps = 0;
+
+	while (num != 1) {
+		if (isEven(num) == 1) {
+            num /= 2;
+			steps++;
+		}
+        else if (isEven(num) == 0) {
+			num *= 3;
+			num++;
+			steps++; 
+		}
+	}
+
+	return steps; 
+}
+
+int main(void) {
+
+	int num;
+
+	printf("Please input a number greater than 1: ");
+	scanf("%d", &num);
+	printf("It took %d steps to apply the Kollatz conjecture to this number.\n", kollatz(num));
+
+	return 0;
+}
